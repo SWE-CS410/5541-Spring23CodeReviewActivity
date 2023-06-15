b3c1256a8431097b7dd1f05e80b3adafdc961fa0
puleri <puleri.mp4@gmail.com>
2023-06-15 14:44:23 -0400

    updated with my code

--- src/main/java/StringManipulation.java
+++ src/main/java/StringManipulation.java
@@ -1,33 +1,117 @@
 public class StringManipulation implements StringManipulationInterface {
+    private String string;
 
-    @Override
     public String getString() {
-        return null;
+        return string;
     }
 
-    @Override
     public void setString(String string) {
+        this.string = string;
     }
 
-    @Override
+    /**
+     * Counts the number of non-empty words in the string.
+     * @return The count of words.
+     */
     public int count() {
-        return 0;
+        String[] words = string.split(" ");
+        int count = 0;
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
-    @Override
+    /**
+     * Removes the character at the nth position in the string.
+     * @param n The position of the character to be removed.
+     * @param maintainSpacing Determines whether to maintain spacing when removing characters.
+     * @return The string with the nth characters removed.
+     */
     public String removeNthCharacter(int n, boolean maintainSpacing) {
-        return null;
+        validateCharacterIndex(n);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < string.length(); i++) {
+            if ((i + 1) % n == 0) {
+                if (maintainSpacing) {
+                    stringBuilder.append(" ");
+                }
+            } else {
+                stringBuilder.append(string.charAt(i));
+            }
+        }
+
+        return stringBuilder.toString();
     }
 
-    @Override
+    /**
+     * Retrieves a substring of words from the specified startWord to the endWord.
+     * @param startWord The starting index of the substring.
+     * @param endWord The ending index of the substring.
+     * @return An array of substrings.
+     */
     public String[] getSubStrings(int startWord, int endWord) {
-        return null;
+        validateWordIndices(startWord, endWord);
+
+        String[] words = string.split(" ");
+        String[] substrings = new String[endWord - startWord + 1];
+        int wordCount = 1;
+        int substringsIndex = 0;
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                if (wordCount >= startWord && wordCount <= endWord) {
+                    substrings[substringsIndex] = word;
+                    substringsIndex++;
+                }
+                wordCount++;
+            }
+        }
+
+        return substrings;
     }
 
-    @Override
+    /**
+     * Restores the characters from the given indices to form a string.
+     * @param indices The indices of the characters to be restored.
+     * @return The restored string.
+     */
     public String restoreString(int[] indices) {
-        return null;
+        validateIndicesLength(indices);
+
+        StringBuilder restoredString = new StringBuilder();
+
+        for (int index : indices) {
+            validateCharacterIndex(index);
+            restoredString.append(string.charAt(index));
+        }
+
+        return restoredString.toString();
     }
 
+    // Private helper methods for input validation
 
+    private void validateCharacterIndex(int index) {
+        if (index <= 0 || index > string.length()) {
+            throw new IndexOutOfBoundsException("Invalid character index");
+        }
+    }
+
+    private void validateWordIndices(int startWord, int endWord) {
+        if (startWord <= 0 || endWord <= 0 || startWord > endWord) {
+            throw new IllegalArgumentException("Invalid word indices");
+        }
+    }
+
+    private void validateIndicesLength(int[] indices) {
+        if (indices.length != string.length()) {
+            throw new IllegalArgumentException("Invalid indices length");
+        }
+    }
 }
