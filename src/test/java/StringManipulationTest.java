ec68ab075056995ef4bd5359f73461b95d6d4cad
Callowlock <chris.nevares@yahoo.com>
2023-06-15 13:06:15 -0700

    changing junit version

--- src/test/java/StringManipulationTest.java
+++ src/test/java/StringManipulationTest.java
@@ -7,15 +7,15 @@
  *
  */
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
+/*import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullEnum;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.ValueSource;*/
 
 
 import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
