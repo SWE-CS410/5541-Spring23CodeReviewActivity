ab8770ccde26d96da512ece2f9f887e434991745
carector <baseline7@live.com>
2023-06-15 11:14:05 -0700

    Update StringManipulationTest import statement

--- src/test/java/StringManipulationTest.java
+++ src/test/java/StringManipulationTest.java
@@ -12,8 +12,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringManipulationTest {
 
