696f3df695f4ea5f9db163f71aebb879ae222918
Brady Six <bpsix646@gmail.com>
2023-06-19 23:47:04 -0700

    Add spaces between comments

--- src/main/java/StringManipulation.java
+++ src/main/java/StringManipulation.java
@@ -30,14 +30,20 @@ public class StringManipulation implements StringManipulationInterface {
     public String removeNthCharacter(int n, boolean maintainSpacing) {
         //check null string
         if(this.string == null) return null;
+
         // check n <= 0
         if(n <= 0) throw new IllegalArgumentException("N must be greater than 0");
+
         // check n > string string
         if(n > this.string.length()) throw new IndexOutOfBoundsException("Index out of bounds");
+
         //edge cases
         if(n == 1 && !maintainSpacing) return "";
         if(n == 1 && maintainSpacing) return this.string.replaceAll(".", " ");
 
+        // builds the string. every multiple of n is either
+        // replaced with whitespace or removed from the result,
+        // depending on the boolean value passed.
         StringBuilder b = new StringBuilder();
         for(int i = 0; i < this.string.length(); i++) {
             if((i+1) % n == 0) {
@@ -53,12 +59,16 @@ public class StringManipulation implements StringManipulationInterface {
     public String[] getSubStrings(int startWord, int endWord){
         //check null string
         if(this.string == null) return null;
+
         //check startWord <= 0
         if(startWord <= 0) throw new IllegalArgumentException("startWord must be greater than zero");
+
         //check endWord <= 0
         if(endWord <= 0) throw new IllegalArgumentException("endWord must be greater than zero");
+
         //check startWord > endWord
         if(startWord > endWord) throw new IllegalArgumentException("startWord cannot be larger than endWord");
+
         //check string.count() < endWord
         if(this.count() < endWord) throw new IndexOutOfBoundsException("endWord cannot be larger than the total word count");
 
@@ -69,12 +79,15 @@ public class StringManipulation implements StringManipulationInterface {
     public String restoreString(int[] indices) {
         //check null string
         if(this.string == null) return null;
+
         //check unequal lengths
         if(this.string.length() != indices.length) throw new IllegalArgumentException("String length must match array length");
+
         //check valid indices
         for(int i = 0; i < indices.length; i++) {
             if(indices[i] < 0 || indices[i] >= this.string.length()) throw new IndexOutOfBoundsException("Invalid array index");
         }
+
         //average case
         StringBuilder b = new StringBuilder();
         for(int i = 0; i < indices.length; i++) {
