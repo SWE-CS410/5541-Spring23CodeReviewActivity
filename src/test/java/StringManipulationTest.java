94b07c4531cd8146a3ea05a98aa313c9fa651762
Ritesh Samal <riteshsamal@outlook.com>
2023-06-19 20:43:08 -0700

    Update

--- src/test/java/StringManipulationTest.java
+++ src/test/java/StringManipulationTest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringManipulationTest {
@@ -28,17 +27,9 @@ public class StringManipulationTest {
 
     @Test
     public void testCount2() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testCount3() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testCount4() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("");
+        int length = manipulatedstring.count();
+        assertEquals(0, length);
     }
 
     @Test
@@ -49,101 +40,103 @@ public class StringManipulationTest {
 
     @Test
     public void testRemoveNthCharacter2() {
-        manipulatedstring.setString("I'd b3tt3r put s0me d161ts in this 5tr1n6, right?");
-        assertEquals("I'  b tt r  ut s0 e  16 ts in th s  tr n6  r gh ?", manipulatedstring.removeNthCharacter(3, true));
+        manipulatedstring.setString("");
+        assertEquals("", manipulatedstring.removeNthCharacter(3, false));
+    }
+
+    @Test
+    public void testGetSubStrings1() {
+        manipulatedstring.setString("This is my string");
+        String[] subStrings = manipulatedstring.getSubStrings(3, 4);
+
+        assertEquals("my", subStrings[0]);
+        assertEquals("string", subStrings[1]);
+    }
+
+    @Test
+    public void testGetSubStrings2() {
+        manipulatedstring.setString("This is my string");
+        String[] subStrings = manipulatedstring.getSubStrings(1, 1);
+
+        assertEquals("This", subStrings[0]);
+    }
+
+    @Test
+    public void testRestoreString1() {
+        manipulatedstring.setString("art");
+        int[] indices = new int[]{1, 0, 2};
+        String restoreString = manipulatedstring.restoreString(indices);
+        assertEquals("rat", restoreString);
+    }
+
+    @Test
+    public void testRestoreString2() {
+        manipulatedstring.setString("");
+        int[] indices = new int[]{};
+        String restoreString = manipulatedstring.restoreString(indices);
+        assertEquals("", restoreString);
+    }
+
+    @Test
+    public void testCount3() {
+        manipulatedstring.setString("12345");
+        int length = manipulatedstring.count();
+        assertEquals(1, length);
+    }
+
+    @Test
+    public void testCount4() {
+        manipulatedstring.setString("One two three four");
+        int length = manipulatedstring.count();
+        assertEquals(4, length);
     }
 
     @Test
     public void testRemoveNthCharacter3() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("Hello, World!");
+        assertEquals("Helo, Wrld!", manipulatedstring.removeNthCharacter(2, false));
     }
 
     @Test
     public void testRemoveNthCharacter4() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("Hello, World!");
+        assertEquals("Helo Wrld", manipulatedstring.removeNthCharacter(2, true));
     }
 
     @Test
-    public void testRemoveNthCharacter5() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testRemoveNthCharacter6() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testRemoveNthCharacter7() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGeSubStrings1() {
+    public void testGetSubStrings3() {
         manipulatedstring.setString("This is my string");
-        String [] sStings = manipulatedstring.getSubStrings(3, 4);
+        String[] subStrings = manipulatedstring.getSubStrings(2, 3);
 
-        assertEquals(sStings[0], "my");
-        assertEquals(sStings[1], "string");
+        assertEquals("is", subStrings[0]);
+        assertEquals("my", subStrings[1]);
     }
 
     @Test
-    public void testGeSubStrings2() {
-        fail("Not yet implemented");
-    }
-    @Test
-    public void testGeSubStrings3() {
-        fail("Not yet implemented");
-    }
-    @Test
-    public void testGeSubStrings4() {
-        fail("Not yet implemented");
-    }
-    @Test
-    public void testGeSubStrings5() {
-        fail("Not yet implemented");
-    }
-    @Test
-    public void testGeSubStrings6() {
-        fail("Not yet implemented");
+    public void testGetSubStrings4() {
+        manipulatedstring.setString("One two three four five");
+        String[] subStrings = manipulatedstring.getSubStrings(1, 5);
+
+        assertEquals("One", subStrings[0]);
+        assertEquals("two", subStrings[1]);
+        assertEquals("three", subStrings[2]);
+        assertEquals("four", subStrings[3]);
+        assertEquals("five", subStrings[4]);
     }
 
     @Test
-    public void testRestoreString1()
-    {
-        manipulatedstring.setString("art");
-        int [] array;
-        array=new int[]{1,0,2};
-        String restoreString = manipulatedstring.restoreString(array);
-        assertEquals(restoreString, "rat");
+    public void testRestoreString3() {
+        manipulatedstring.setString("abc");
+        int[] indices = new int[]{2, 0, 1};
+        String restoreString = manipulatedstring.restoreString(indices);
+        assertEquals("bca", restoreString);
     }
 
     @Test
-    public void testRestoreString2()
-    {
-        fail("Not yet implemented");
-
+    public void testRestoreString4() {
+        manipulatedstring.setString("hello");
+        int[] indices = new int[]{4, 3, 2, 1, 0};
+        String restoreString = manipulatedstring.restoreString(indices);
+        assertEquals("olleh", restoreString);
     }
-
-    @Test
-    public void testRestoreString3()
-    {
-        fail("Not yet implemented");
-
-    }
-
-    @Test
-    public void testRestoreString4()
-    {
-        fail("Not yet implemented");
-
-    }
-
-    @Test
-    public void testRestoreString5()
-    {
-        fail("Not yet implemented");
-
-    }
-
 }
